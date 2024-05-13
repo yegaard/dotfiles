8399cad4dd961256aa0be3509ca319bdfd2e5bbf
Yegor Uskov <yegaard.uskoff@gmail.com>
2024-05-13 19:44:30 +0300

    some improvements

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+--NeoVim Config--
+
+--show line numbers
+vim.o.number = true
+
+--synchronize registers with system clipboard
+vim.o.clipboard = 'unnamedplus'
+
+--number of spaces in tabs
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+--enable mouse support 
+vim.o.mouse = 'a'
